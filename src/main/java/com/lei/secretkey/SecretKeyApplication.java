b952e73b90d090f260a5af8545bdfcac8cfa0e16
yunlonglei <yunlonglei0@gmail.com>
2022-01-14 16:35:03 +0800

    Initial commit

---
+++ src/main/java/com/lei/secretkey/SecretKeyApplication.java
@@ -0,0 +1,13 @@
+package com.lei.secretkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretKeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretKeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lei/secretkey/SecretKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lei.secretkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretKeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
