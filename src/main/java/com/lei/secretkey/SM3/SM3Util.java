bcab827a682a515067f59cba99a5473b67e5d1f5
yunlonglei <yunlonglei0@gmail.com>
2022-03-24 11:10:03 +0800

    feat: 添加新功能，SM3加密方式
    1. sm3算法加密-无密钥加密。 pass
    2. sm3算法加密-密钥加密。能加密，无法验证密码。
    
    提交人：lei

---
+++ src/main/java/com/lei/secretkey/SM3/SM3Util.java
@@ -0,0 +1,120 @@
+package com.lei.secretkey.SM3;
+
+import org.bouncycastle.crypto.digests.SM3Digest;
+import org.bouncycastle.crypto.macs.HMac;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.pqc.math.linearalgebra.ByteUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.Security;
+import java.util.Arrays;
+
+/**
+ * SM3加密算法工具类（不可逆算法）
+ * @author leiyunlong
+ * @date 2022/3/24 10:31
+ * @since 1.0
+ */
+public class SM3Util {
+    private static final String ENCODING = "UTF-8";
+
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    /**
+     * sm3算法加密
+     *
+     * @param paramStr 待加密字符串
+     * @return 返回加密后，固定长度=32的16进制字符串
+     * @explain 无密钥
+     */
+    public static String encrypt(String paramStr) {
+        // 将返回的hash值转换成16进制字符串
+        String resultHexString = "";
+        try {
+            // 将字符串转换成byte数组
+            byte[] srcData = paramStr.getBytes(ENCODING);
+            // 调用hash()
+            byte[] resultHash = hash(srcData);
+            // 将返回的hash值转换成16进制字符串
+            resultHexString = ByteUtils.toHexString(resultHash);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return resultHexString;
+    }
+
+    /**
+     * 返回长度=32的byte数组
+     *
+     * @param srcData 参数
+     * @return 32的byte数组
+     * @explain 生成对应的hash值
+     */
+    public static byte[] hash(byte[] srcData) {
+        SM3Digest digest = new SM3Digest();
+        digest.update(srcData, 0, srcData.length);
+        byte[] hash = new byte[digest.getDigestSize()];
+        digest.doFinal(hash, 0);
+        return hash;
+    }
+
+    /**
+     * 通过密钥进行加密
+     *
+     * @param key     密钥
+     * @param srcData 被加密的byte数组
+     * @return 加密结果
+     * @explain 指定密钥进行加密
+     */
+    public static byte[] hmac(byte[] key, byte[] srcData) {
+        KeyParameter keyParameter = new KeyParameter(key);
+        SM3Digest digest = new SM3Digest();
+        HMac mac = new HMac(digest);
+        mac.init(keyParameter);
+        mac.update(srcData, 0, srcData.length);
+        byte[] result = new byte[mac.getMacSize()];
+        mac.doFinal(result, 0);
+        return result;
+    }
+
+    /**
+     * 判断源数据与加密数据是否一致
+     *
+     * @param srcStr       原字符串
+     * @param sm3HexString 16进制字符串
+     * @return 校验结果
+     * @explain 通过验证原数组和生成的hash数组是否为同一数组，验证2者是否为同一数据
+     */
+
+    public static boolean verify(String srcStr, String sm3HexString) {
+        boolean flag = false;
+        try {
+            byte[] srcData = srcStr.getBytes(ENCODING);
+            byte[] sm3Hash = ByteUtils.fromHexString(sm3HexString);
+            byte[] newHash = hash(srcData);
+            if (Arrays.equals(newHash, sm3Hash)) {
+                flag = true;
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        //测试json
+        String json = "{\"name\":\"Marydon\",\"website\":\"http://www.cnblogs.com/Marydon20170307\"}";
+
+        // 无密钥加密
+        String hex = encrypt(json);
+        //0b0880f6f2ccd817809a432420e42b66d3772dc18d80789049d0f9654efeae5c
+        System.out.println(hex);
+        //验证加密后的16进制字符串与加密前的字符串是否相同
+        boolean flag = verify(json, hex);
+        // 验证结果 true
+        System.out.println(flag);
+    }
+}
